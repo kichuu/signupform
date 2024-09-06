d70dacdc89c73c99c8a83f2f908a00b32a7569e3
kichuu <krishnadevkichus@gmail.com>
2024-09-06 18:59:10 +0530

    js function for alert message added

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+const submitMessage = document.querySelector("#submit-button");
+
+submitMessage.addEventListener("click", (e) => {
+    alert("enit poda Signup cheyan vanekan avan");
+    e.preventDefault()
+});
